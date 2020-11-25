9f67da19303202a70bb2844776fa319e7cae8271
Rai Mota <74794862+raimota@users.noreply.github.com>
2020-11-25 17:32:41 -0300

    Add files via upload

---
+++ exe_02.java
@@ -0,0 +1,45 @@
+﻿import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        try (Scanner leia = new Scanner(System.in);) {
+            boolean recebeB = true;
+            int qtd;
+            System.out.printf("Digite um número positivo maior que zero: ");
+            qtd = leia.nextInt();
+            while (qtd <= 0) {
+                System.out.printf("Digite um número positivo maior que zero: ");
+                qtd = leia.nextInt();
+            }
+            String b[] = new String[qtd];
+            String ast[] = new String[qtd];
+            String res[][] = new String[qtd][qtd];
+            for (int i = 0; i < qtd * 2; i++) {
+                for (int j = 0; j < qtd; j++) {
+                    if (i < qtd) {
+                        b[j] = "o";
+                    } else {
+                        ast[j] = "*";
+                    }
+                }
+            }
+            System.out.println();
+            for (int i = 0; i < qtd; i++) {
+                if(i % 2 == 0){
+                    recebeB = true;
+                }else{
+                    recebeB = false;
+                }
+                for (int j = 0; j < qtd; j++) {
+                    if (recebeB == true) {
+                        System.out.print(res[i][j] = b[j]);
+                        recebeB = false;
+                    } else {
+                        System.out.print(res[i][j] = ast[j]);
+                        recebeB = true;
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
